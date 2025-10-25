cce911511f7c0891332e494b9801dfe8df425623
Rogerio Martins <rogerio.martins@ima.sp.gov.br>
2025-10-25 10:14:39 -0300

    Ingestão do PDF -> OK

--- src/ingest.py
+++ src/ingest.py
@@ -6,30 +6,42 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_core.documents import Document
 from langchain_postgres import PGVector
 
+# Carregar variáveis de ambiente
 load_dotenv()
 for k in ("OPENAI_API_KEY", "DATABASE_URL","PGVECTOR_COLLECTION", "PDF_PATH"):
     if not os.getenv(k):
         raise RuntimeError(f"Environment variable {k} is not set")
 
-
+# Caminho do PDF a ser ingerido
 PDF_PATH = os.environ["PDF_PATH"]
 
 def ingest_pdf():
+    # Carrega o PDF
     loader = PyPDFLoader(PDF_PATH)
     documents = loader.load()
+    
+    # Divide o documento em pedaços menores
     splits = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150, add_start_index=False).split_documents(documents)
     if not splits:
-        raise RuntimeError("No document splits were created from the PDF.")
-    print(f"Created {len(splits)} document splits from the PDF.")
+        raise RuntimeError("Erro: não foram criados os pedaços do PDF.")
+    print(f"Criados {len(splits)} pedaços do PDF.")
+    
+    # Enriquecer com metadados
     enriched = [
-    Document(
-        page_content=d.page_content,
-        metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
-    )
-    for d in splits
+        Document(
+            page_content=d.page_content,
+            metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+        )
+        for d in splits
     ]
+
+    # Criar os índices para cada pedaço
     ids = [f"doc-{i}" for i in range(len(enriched))]
+    
+    # Criar os vetores
     embeddings = OpenAIEmbeddings(model=os.getenv("OPENAI_MODEL","text-embedding-3-small"))
+    
+    # Armazenar no PGVector
     store = PGVector(
         embeddings=embeddings,
         collection_name=os.environ["PGVECTOR_COLLECTION"],
@@ -37,8 +49,6 @@ def ingest_pdf():
         use_jsonb=True,
     )
     store.add_documents(documents=enriched, ids=ids)
-    
-
 
 if __name__ == "__main__":
     ingest_pdf()
